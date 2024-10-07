85b3fe4ee95eee0f0bdddab7c5d88ec81e0712b6
Jordan <99503935+jbiehl88@users.noreply.github.com>
2024-10-07 15:07:50 -0500

    added some styling to item component

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -2,9 +2,12 @@ import React from "react"
 
 export const Item = ({ item }) => {
 	return (
-		<>
-			<h3>{item.name}</h3>
-			<img src={item.image} alt={item.name} />
-		</>
+		<div className="item-display">
+			<div className="item-card">
+				<img className="image-size" src={item.image} alt={item.name} />
+				<h3>{item.name}</h3>
+				<p>{item.price}</p>
+			</div>
+		</div>
 	)
 }
