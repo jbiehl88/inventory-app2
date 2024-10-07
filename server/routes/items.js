c7e0512b83f3e6a1c7d8509e6463bc720db5db25
Jordan <99503935+jbiehl88@users.noreply.github.com>
2024-10-07 13:57:19 -0500

    created Item get all route

---
+++ server/routes/items.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const itemRouter = express.itemRouter()
+const { Item } = require("../models")
+
+// GET /items
+itemRouter.get("/", async (req, res, next) => {
+	try {
+		const items = await Item.findAll()
+		res.send(items)
+	} catch (error) {
+		next(error)
+	}
+})
+
+module.exports = router
