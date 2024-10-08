3454d71fc695cb09c98f67c3ed8da60210fa54eb
Tiffany <139176913+talph101@users.noreply.github.com>
2024-10-08 10:20:51 -0500

    Added handle click and handle click functioms for PopUp component.

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -1,10 +1,25 @@
-import React from "react"
+import React, { useState } from "react";
+// add single view to Item (Tiff)
+import { PopUp } from "react";
 
 export const Item = ({ item }) => {
 	const currency = item.price.toFixed(2)
+	//added
+	const [isImgClicked, setIsImageClicked] = useState(false);
+
+	function handleImgClick() {
+		setIsImageClicked(true);
+	}
+
+	function handleClose() {
+		setIsImageClicked(false);
+	}
+
+	
 	return (
 		<div className="item-card">
-			<img className="image-size" src={item.image} alt={item.name} />
+			<img className="image-size" src={item.image} alt={item.name} onClick={handleImgClick}/>
+			{/* {isImgClicked && <PopUp item ={item} onClose={handleClose}/>} */}
 			<h3 className="item-name">{item.name}</h3>
 			<p className="item-price">${currency}</p>
 		</div>
---
+++ public/react/components/PopUp.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export const PopUp = ({ item }) => {
+    return (
+        // <div className="item-card">
+        //     <ul>
+        //         <li>Name:{item.name}</li>
+        //         <li>Price:{item.price}</li>
+        //         <li>Category:{item.category}</li>
+        //         <li>Description:{item.description}</li>
+        //     </ul>
+        // </div>
+    )
+}
+
+//the reason why you can have a destructed prop in here is because the component is being called elsewhere. This component is being called in Item??
