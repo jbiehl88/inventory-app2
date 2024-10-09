9dbd00cc05e5e0f7caae53016b4a0ce6ebfb4380
Jordan <99503935+jbiehl88@users.noreply.github.com>
2024-10-09 10:03:26 -0500

    fixed reference error bug
    -Order model was throwing reference error. Discovered it was due to line 6 calling User.init instead of Order.init

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,12 @@
-const { Sequelize } = require("sequelize");
-const { sequelize } = require("../db");
-const { Item } = require("./Item");
-const { User } = require("./User");
-const { Order } = require("./Order");
+const { Sequelize } = require("sequelize")
+const { sequelize } = require("../db")
+const { Item } = require("./Item")
+const { User } = require("./User")
+const { Order } = require("./Order")
 
 module.exports = {
-  db: sequelize,
-  Item,
-  User,
-  Order,
-};
+	db: sequelize,
+	Item,
+	User,
+	Order,
+}
