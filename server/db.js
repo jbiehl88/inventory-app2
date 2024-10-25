6390a2066beb760aa511a67e1e13a2aa7a7c4f76
Jordan <99503935+jbiehl88@users.noreply.github.com>
2024-10-25 11:44:21 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const sequelize = new Sequelize(process.env.REACT_APP_DB_NAME, process.env.REACT_APP_DB_USER, process.env.REACT_APP_DB_PASS, {
     host: process.env.REACT_APP_DB_HOST,
+    port: process.env.REACT_APP_DB_PORT,
     dialect: 'postgres',        
   });
 
