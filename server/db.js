8f133fb3224fe4006efd7e3e3a293ead480d0cab
Jordan <99503935+jbiehl88@users.noreply.github.com>
2024-10-25 09:42:30 -0500

    removed dotenv req

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
 const path = require('path');
-require('dotenv').config();
+
 
 const sequelize = new Sequelize(process.env.REACT_APP_DB_NAME, process.env.REACT_APP_DB_USER, process.env.REACT_APP_DB_PASS, {
     host: process.env.REACT_APP_DB_HOST,
