ae912f1660c2da909008628d4bb3c8780e482c74
Jordan <99503935+jbiehl88@users.noreply.github.com>
2024-10-25 10:17:00 -0500

    api adjustment

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
-import { REACT_APP_API_SERVER } from 'react-dotenv';
+// import { REACT_APP_API_SERVER } from 'react-dotenv';
 
 let apiURL;
 
 if (process.env.NODE_ENV === 'development') {
   apiURL =  'http://localhost:3000/api';
 } else {
-  apiURL = `${REACT_APP_API_SERVER}:3000/api`
+  apiURL = `${process.env.REACT_APP_API_SERVER}:3000/api`
 }
 
 export default apiURL;
